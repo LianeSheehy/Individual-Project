959feba6a4e6233fdbc1d47c38412d2e51212f32
LianeSheehy <32622617+LianeSheehy@users.noreply.github.com>
2019-04-14 17:43:31 +0100

    URLS

---
+++ urls.py
@@ -0,0 +1,49 @@
+"""
+Definition of urls for DjangoWebProject1.
+"""
+
+from datetime import datetime
+from django.conf.urls import url
+from django.urls import path
+import django.contrib.auth.views
+
+import app.forms
+import app.views
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    path('api/barchart_data', app.views.barchart_data),
+    url(r'^$', app.views.home, name='home'),
+    url(r'^contact$', app.views.contact, name='contact'),
+    url(r'^about$', app.views.about, name='about'),
+    url(r'^callRStudioTest$', app.views.callRStudioTest,name="callRStudioTest"),
+    url(r'^d3js_barchart$', app.views.d3js_barchart, name='d3js_barchart'),
+    url(r'^login/$', django.contrib.auth.views.LoginView,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': app.forms.BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title': 'Log in',
+                'year': datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.LogoutView,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
